ba0fb5bf9eacdb725b099d6439e1dc2735ede10f
Amogh-007-Rin <amoghdath233@gmail.com>
2025-12-04 16:00:14 +0000

    Added-new-classes

--- OhMyFarm/src/app/MyFarm.java
+++ OhMyFarm/src/app/MyFarm.java
@@ -4,15 +4,17 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+
+
 public class MyFarm {
 	public static void main(String[] args) {
 		// Run me
 		displayTask();
-		
+
 	}
 	
 	public static void displayTask() {
-	    try (BufferedReader reader = new BufferedReader(new FileReader("task.txt"))) {
+	    try (BufferedReader reader = new BufferedReader(new FileReader("OhMyFarm\\animals.txt"))) {
 	        String line;
 	        while ((line = reader.readLine()) != null) {
 	            System.out.println(line);
@@ -22,4 +24,5 @@ public class MyFarm {
 	    }
 	}
 
+	
 }
